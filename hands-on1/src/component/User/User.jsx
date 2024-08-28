8bcbb9e6e0bbb3a4685da7c9fc71522749a45ee1
MaEricaCruz <cruzmaerica982@gmail.com>
2024-08-28 23:03:53 +0800

    my reactjs app

---
+++ hands-on1/src/component/User/User.jsx
@@ -0,0 +1,12 @@
+function User(){
+    return(
+       <div> 
+           <h1> Ma. Erica J. Cruz</h1>
+           <h2> BSIT - 3B</h2>
+           <p>  Description</p>
+   
+       </div>
+    )
+   }
+   
+   export default User;
