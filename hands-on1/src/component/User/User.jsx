ef66ab72cf98d6a7c89757f32c356186237e93ba
MaEricaCruz <cruzmaerica982@gmail.com>
2024-10-31 20:12:15 +0800

    ADT

---
+++ hands-on1/src/component/User/User.jsx
@@ -0,0 +1,12 @@
+function User(){
+    return(
+       <div> 
+           <h1> Ma. Erica J. Cruz</h1>
+           <h2> BSIT - 3B</h2>
+           <p>  I am a IT student</p>
+   
+       </div>
+    )
+   }
+   
+   export default User;
